2b4574e7a71630ed92e04e757d41efd218cb9b89
Jakub Sydor <sydorjakub@gmail.com>
2023-01-19 16:44:23 +0100

    feat: add app config

---
+++ src/config/app.config.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppConfig {
+  constructor(private readonly config: ConfigService) {}
+
+  /**
+   * The API key to use for the API.
+   */
+  get apiKey(): string {
+    return this.config.get('API_KEY', 'DEMO_KEY');
+  }
+
+  /**
+   * Maximum number of concurrent requests to the API.
+   */
+  get apiConcurrentRequest(): number {
+    return this.config.get('CONCURRENT_REQUESTS', 5);
+  }
+
+  /**
+   * The API key to use for the API.
+   */
+  get appPort(): number {
+    return this.config.get('PORT', 8080);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { AppConfig } from './config';
 import { HttpExceptionFilter } from './core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const appConfig = new AppConfig(configService);
 
   // 1. Enable global pipes
   app.useGlobalPipes(new ValidationPipe());
@@ -12,6 +17,7 @@ async function bootstrap() {
   // 2. Enable global filters
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(appConfig.appPort);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
